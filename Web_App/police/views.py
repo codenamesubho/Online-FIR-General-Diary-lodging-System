33469951295c15f559b532d948a375f2fc8cf166
Ghost-script <subho.prp@gmail.com>
2015-04-26 14:49:58 +0530

    Modified decorators

--- Web_App/police/views.py
+++ Web_App/police/views.py
@@ -3,25 +3,36 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from Web_App.models import Fir
 from functools import wraps
+from django.http import HttpResponse
 # Create your views here.
 
+"""
 def police_login_required(login_url):
     def innerfunc(f):    
         @wraps(f)
         def wrap(request, *args, **kwargs):
             #this check the session if userid key exist, if not it will redirect to login page
-            print request.session.keys(),request.session.values()
             if '_auth_user_id' not in request.session.keys() or 'is_police' not in request.session.keys():
                     return redirect(login_url)
             return f(request, *args, **kwargs)
         return wrap
     return innerfunc
+"""
+
+def verify_police(func):
+    @wraps(func)
+    def wrapper(request,*args,**kwargs):
+        if 'is_police' not in request.session.keys():
+            return HttpResponse("Not Authorized")
+        return func(request,*args,**kwargs)
+    return wrapper
 
 
 def index(request):
 	return render(request,'police/index.html')
 
-@police_login_required(login_url='/police/login')
+@login_required(login_url='/police/login')
+@verify_police
 def dashboard(request):
     lastfir = Fir.objects.order_by('-pub_date')[:5]
     return render(request,'police/dashboard.html',{'Fir' : lastfir})
@@ -39,6 +50,8 @@ def login_user(request):
     else:
 		return redirect('police_login')
 
+
+@login_required(login_url='/police/login')
 def logout_user(request):
     logout(request)
     return redirect('police_login')
--- Web_App/users/views.py
+++ Web_App/users/views.py
@@ -3,18 +3,32 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from Web_App.models import Fir
 from django.utils import timezone
+from functools import wraps
+from django.http import HttpResponse
+
+def is_user(func):
+    @wraps(func)
+    def wrapper(request,*args,**kwargs):
+        if 'is_user' not in request.session.keys():
+            return HttpResponse("Not Authorized")
+        return func(request,*args,**kwargs)
+    return wrapper
 
 def index(request):
 	return render(request,'users/index.html')
 
 @login_required(login_url='/')
+@is_user
 def dashboard(request):
 	return render(request,'users/dashboard.html',{ 'active' : 'dashboard' })
 
 @login_required(login_url='/')
+@is_user
 def lodge(request):
     return render(request,'users/new.html',{ 'active' : 'new' })
 
+@login_required(login_url='/')
+@is_user
 def register_fir(request):
     title = request.POST['title']
     detail = request.POST['detail']
@@ -33,10 +47,13 @@ def login_user(request):
     if user is not None:
         if user.is_active:
             login(request, user)
+            request.session['is_user'] = True
             return redirect('lodge_new')
     else:
 		return redirect('home')
 
+
+@login_required(login_url='/')
 def logout_user(request):
     logout(request)
     return redirect('home')
